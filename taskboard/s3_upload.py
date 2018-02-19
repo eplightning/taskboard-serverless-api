c0dba32e4bb0d23443b62f8c90f9c888af620c75
Bartosz Sławianowski <wrexdot@gmail.com>
2018-02-19 01:17:22 +0100

    Fix encoding issue with S3 upload

--- taskboard/s3_upload.py
+++ taskboard/s3_upload.py
@@ -1,11 +1,12 @@
 import boto3
 from taskboard.models import Task
+from urllib.parse import unquote_plus
 
 s3 = boto3.client('s3', 'eu-west-1')
 
 def process_upload(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
+    key = unquote_plus(event['Records'][0]['s3']['object']['key'])
 
     head = s3.head_object(Bucket=bucket, Key=key)
     meta = head['Metadata']
